0ce616340b707cbb9422ad1da501170aa262f5ca
Anna <annamurray278@gmail.com>
2024-10-10 01:03:02 -0500

    buggy 3.12

--- software/apps/gpio/gpio.c
+++ software/apps/gpio/gpio.c
@@ -1,9 +1,25 @@
 #include "gpio.h"
+#include <stdio.h>
 
 typedef struct{
   // Step 3:
   // Add register definitions here
+  uint32_t _unused0[0x504/4];
+  uint32_t OUT;
+  uint32_t OUTSET;
+  uint32_t OUTCLR;
+  uint32_t IN;
+  uint32_t DIR;
+  uint32_t DIRSET;
+  uint32_t DIRCLR;
+  uint32_t LATCH;
+  uint32_t DETECTMODE;
+  uint32_t _unused1[(0x700-0x524)/4-1];
+  uint32_t PIN_CNF[32];
 } gpio_reg_t;
+
+volatile gpio_reg_t *port_0 = (gpio_reg_t*)0x50000000;
+volatile gpio_reg_t *port_1 = (gpio_reg_t*)0x50000300;
 
 // Inputs: 
 //  gpio_num - gpio number 0-31 OR (32 + gpio number)
@@ -12,6 +28,25 @@ void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
   // Implement me
   // This function should configure the pin as an input/output
   // Hint: Use proper PIN_CNF instead of DIR
+  uint32_t port = gpio_num >> 5;
+  uint32_t gpio_pin = gpio_num & 0x1F;
+  if (port == 0) {
+    if (!dir) {
+      // printf("gpio_config port 0 input\n");
+      port_0->PIN_CNF[gpio_pin] = 0;
+    }
+    else {
+      port_0->PIN_CNF[gpio_pin] = 3;
+    }
+  } else {
+    if (!dir) {
+      port_1->PIN_CNF[gpio_pin] = 0;
+    }
+    else {
+      port_1->PIN_CNF[gpio_pin] = 3;
+    }
+  }
+  printf("port0->PIN_CNF: %ld\n", port_1->PIN_CNF[gpio_pin]);
 }
 
 // Inputs: 
@@ -20,6 +55,16 @@ void gpio_set(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin high
   // It can assume that the pin has already been configured
+  uint32_t gpio_pin = gpio_num & 0x1F;
+  uint32_t port = gpio_num >> 5;
+
+  printf("gpio_pin: %ld, port: %ld\n", gpio_pin, port);
+  if (!port) {
+    port_0->OUTSET = 1 << gpio_pin;
+  } else {
+    port_1->OUTSET = 1 << gpio_pin;
+  }
+  printf("outset: %d\n", 1 << gpio_pin);
 }
 
 // Inputs: 
@@ -28,6 +73,13 @@ void gpio_clear(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin low
   // It can assume that the pin has already been configured
+  uint32_t gpio_pin = gpio_num & 0x1F;
+  uint32_t port = gpio_num >> 5;
+  if (!port) {
+    port_0->OUTCLR = 1 << gpio_pin;
+  } else {
+    port_1->OUTCLR = 1 << gpio_pin;
+  }
 }
 
 // Inputs: 
@@ -38,8 +90,13 @@ bool gpio_read(uint8_t gpio_num) {
   // Implement me
   // This function should read the value from the pin
   // It can assume that the pin has already been configured
-
-  return true;
+  uint32_t gpio_pin = gpio_num & 0x1F;
+  uint32_t port = gpio_num >> 5;
+  if (!port) {
+    return port_0->IN & (1 << gpio_pin);
+  } else {
+    return port_1->IN & (1 << gpio_pin);
+  }
 }
 
 // prints out some information about the GPIO driver. Can be called from main()
@@ -47,5 +104,7 @@ void gpio_print(void) {
   // Use this function for debugging purposes
   // For example, you could print out struct field addresses
   // You don't otherwise have to write anything here
+  // printf("port_0_DIR: %p\tport_0_pincnf[18]: %p\n", &port_0->DIR, &port_0->PIN_CNF[18]);
+  // printf("%d", gpio_read(22));
 }
 
--- software/apps/gpio/main.c
+++ software/apps/gpio/main.c
@@ -17,13 +17,34 @@ int main(void) {
 
   // Turn on all LEDs on the back of the Microbit
   // Add code here
+  gpio_config(21, 0);
+  gpio_config(22, 0);
+  gpio_config(15, 0);
+  gpio_config(24, 0);
+  gpio_config(19, 0);
+  gpio_config(28, 0);
+  gpio_config(11, 0);
+  gpio_config(31, 0);
+  gpio_config(37, 0);
+  gpio_config(30, 0);
+
+  gpio_set(21);
+  gpio_set(22);
+  gpio_set(15);
+  gpio_set(24);
+  gpio_set(19);
+  gpio_clear(28);
+  gpio_clear(11);
+  gpio_clear(31);
+  gpio_clear(37);
+  gpio_clear(30);
 
   // Control LED with raw MMIO
   // Microphone LED is P0.20 and active high
   // Add code here
   *(uint32_t*)(0x50000514) = 0x00100000; // DIR
   *(uint32_t*)(0x50000504) = 0x00100000; // OUT
-  
+
 
 
   // loop forever
@@ -34,6 +55,10 @@ int main(void) {
     // Button A is P0.14 and active low
     // Button B is P0.23 and active low
     // Add code here
+
+    gpio_print();
+
+    
 
     nrf_delay_ms(100);
   }
